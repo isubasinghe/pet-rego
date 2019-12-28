68932399daab66b3ef4a0ad7954e966e392422d9
Isitha Subasinghe <isitha@MacBookProLocal>
2019-12-28 20:14:45 +1100

    Added ability to create a pet

--- src/pets/pets.controller.ts
+++ src/pets/pets.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { PetsService } from './pets.service';
+import { CreatePetDTO } from './dtos/create-pet.dto';
 
 @Controller('pets')
 export class PetsController {
   constructor(private readonly petService: PetsService) {}
 
   @Post()
-  addPet() {}
+  addPet(@Body() pet: CreatePetDTO) {
+    return this.petService.create(pet);
+  }
 }
